d0ca17dd43efe51c87ff5f36566e6e7921538668
Danilo Gomanjuk <danilogomanjuk93@gmail.com>
2026-05-22 15:39:40 +0200

    fix: prevent scroll value change on number inputs & add db backup to update script

--- update-mesara.bat
+++ update-mesara.bat
@@ -3,20 +3,30 @@ echo ===================================================
 echo   ZAPOCINJEM AUTOMATSKO AZURIRANJE MESARA ERP-A
 echo ===================================================
 
-echo [1/4] Povlacim najnoviji kod sa Git-a...
+echo [1/5] Pravim sigurnosnu kopiju (backup) baze podataka...
+if not exist backups mkdir backups
+
+:: Generisanje datuma i vremena za naziv fajla (npr. 20260522_1415)
+set "timestamp=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~0,2%%time:~3,2%"
+set "timestamp=%timestamp: =0%"
+
+docker exec mesara-mysql mysqldump -u DB_USER -pDB_PASSWORD DB_NAME > backups\backup_%timestamp%.sql
+
+echo Backup uspesan! Sacuvan u folder: backups\backup_%timestamp%.sql
+
+echo [2/5] Povlacim najnoviji kod sa Git-a...
 git pull origin main
 
-echo [2/4] Gasim trenutne kontejnere (Tvoja baza i pazar ostaju sacuvani)...
+echo [3/5] Gasim trenutne kontejnere (Tvoja baza i pazar ostaju sacuvani)...
 docker-compose down
 
-echo [3/4] Bildujem i podizem novu verziju (Ovo moze potrajati par minuta)...
+echo [4/5] Bildujem i podizem novu verziju (Ovo moze potrajati par minuta)...
 docker-compose up -d --build
 
-echo [4/4] Cistim stare slike da oslobodim prostor na disku...
+echo [5/5] Cistim stare slike da oslobodim prostor na disku...
 docker image prune -f
 
 echo ===================================================
-echo   NOVA VERZIJA JE USPESNO PODIGNUTA!
-echo   Pristupi na: http://localhost:8080
+echo   NOVA VERZIJA JE USPESNO PODIGNUTA I BAZA JE SACUVANA!
 echo ===================================================
 pause
